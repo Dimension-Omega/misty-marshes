ceca205cdf19bbd6127d94ae18efa6712533ec31
nicktheway <nikospanwkatw96@gmail.com>
2022-01-22 04:01:14 +0200

    Gate and frog trigger

--- data/elements/frog/frog.gd
+++ data/elements/frog/frog.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+export var trigger_node : NodePath
 
 func duality(color: String) -> void:
 	var _isWhite := color == 'white'
@@ -8,3 +9,8 @@ func duality(color: String) -> void:
 func interact() -> void:
 	if $FrogBlack.visible:
 		$AnimationPlayer.play("InteractBlack")
+	else:
+		var triggerNode = get_node(trigger_node)
+		if is_instance_valid(triggerNode) and triggerNode.is_inside_tree():
+			assert(triggerNode.has_method('trigger'))
+			triggerNode.trigger()
---
+++ data/elements/gates/gate.gd
@@ -0,0 +1,24 @@
+tool
+extends KinematicBody2D
+
+enum EPosition {HORIZONTAL = 0, VERTICAL = 1}
+export(EPosition) var initial_pos = EPosition.HORIZONTAL setget set_initial_pos
+
+var gate_position : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_gate_position(initial_pos, true)
+
+func trigger() -> void:
+	set_gate_position(EPosition.HORIZONTAL if gate_position == EPosition.VERTICAL else EPosition.VERTICAL)
+
+func set_gate_position(gatePosition: int, instant: bool = false) -> void:
+	$AnimationPlayer.play("horizontal" if gatePosition == EPosition.HORIZONTAL else "vertical")
+	if instant:
+		$AnimationPlayer.seek(2, true)
+	gate_position = gatePosition
+	
+func set_initial_pos(value: int) -> void:
+	initial_pos = value
+	rotation_degrees = -90 if initial_pos == EPosition.VERTICAL else 0
