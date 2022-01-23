d0c9c0e4a608b7d8e8d73c99e36c96ff4fe914ae
nicktheway <nikospanwkatw96@gmail.com>
2022-01-23 17:18:23 +0200

    Secret acquire effect and set secret + targets in all levels

--- data/elements/secret/secret.gd
+++ data/elements/secret/secret.gd
@@ -4,4 +4,8 @@ extends Area2D
 func _on_Secret_body_entered(body):
 	if body.has_meta('player'):
 		get_tree().call_group('main', 'on_secret_found')
+		$AnimationPlayer.play("acquired")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == 'acquired':
 		queue_free()
