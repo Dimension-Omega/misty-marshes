8a45fa31444d28522ea865a1f20ef416614a9d19
nicktheway <nikospanwkatw96@gmail.com>
2022-01-23 22:41:55 +0200

    Fix mute button text on main menu

--- data/menu/MainMenu.gd
+++ data/menu/MainMenu.gd
@@ -10,6 +10,7 @@ onready var level_buttons = [
 	$LevelsPanel/GridContainer/Level2/MarginContainer/HBoxContainer/Level_2,
 	$LevelsPanel/GridContainer/Level3/MarginContainer/HBoxContainer/Level_3,
 ]
+onready var mute_button = $MuteButton
 const AUDIO_MASTER_BUS := 0
 
 var main 
@@ -19,20 +20,21 @@ func _ready() -> void:
 	if mainGroup:
 		main = mainGroup[0]
 	levels_button.grab_focus()
-	
 	if main:
 		for i in range(level_buttons.size()):
 			var levelButton : Button = level_buttons[i]
 			if not levelButton.is_connected("pressed", main, 'load_level_and_set_world'):
 				var _conErr = levelButton.connect("pressed", main, 'load_level_and_set_world', [i+1])
-				
 	if ProjectSettings.get_setting('application/config/is_html'):
 		$Buttons/VBoxContainer/LevelsButton.focus_neighbour_top = "../Credits"
 		$Buttons/VBoxContainer/LevelsButton.focus_previous = "../Credits"
 		$Buttons/VBoxContainer/Credits.focus_neighbour_bottom = "../LevelsButton"
 		$Buttons/VBoxContainer/Credits.focus_next = "../LevelsButton"
 		$Buttons/VBoxContainer/Exit.queue_free()
-	
+	refresh_mute_button()
+
+func refresh_mute_button() -> void:
+	mute_button.text = 'Unmute' if AudioServer.is_bus_mute(AUDIO_MASTER_BUS) else 'Mute'
 
 func _on_Exit_pressed():
 	get_tree().quit(0)
@@ -61,10 +63,9 @@ func _on_Help_pressed():
 	buttons.visible = false
 	last_panel = 'help'
 
-
 func _on_MuteButton_pressed():
 	AudioServer.set_bus_mute(AUDIO_MASTER_BUS, not AudioServer.is_bus_mute(AUDIO_MASTER_BUS))
-
+	refresh_mute_button()
 
 func _on_LicensesPopupCloseButton_pressed():
 	$CreditsPanel/LicensesPopup.visible = false
